2e481285f4f0071e80cf0e395d8522f8d0b70f55
olagajkowska <67837503+olagajkowska@users.noreply.github.com>
2021-02-19 23:02:13 +0100

    small update of whole.py

--- Whole.py
+++ Whole.py
@@ -1,7 +1,28 @@
-from Theory import Theory
-from Experiment import Experiment
-from Tools import *
-
 
 class Whole:
-    pass
+
+    def __init__(self, number_of_traces):
+
+        if number_of_traces == 1:
+            raise ValueError
+
+        self._traces = self._collect_traces()
+        self.p_mean = sum([trace.p for trace in self._traces])/number_of_traces
+        self.k_mean = sum([trace.k for trace in self._traces]) / number_of_traces
+
+    def _collect_traces(self):
+        raise NotImplemented()
+
+    def _compute_all(self):
+        for trace in range(len(self._traces)):
+            self._traces[trace].calculate_work()
+            self._traces[trace].rupture_forces()
+
+    def plot_all(self):
+        for trace in range(len(self._traces)):
+            self._traces[trace].plot()
+
+    def save_all(self):
+        for trace in range(len(self._traces)):
+            self._traces[trace].results_to_latex(self._traces[trace].histo_data)
+            self._traces[trace].results_to_txt()
